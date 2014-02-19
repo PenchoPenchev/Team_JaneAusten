b9855c40f4ffb5290796f62167180a3c9b866283
LachezarTomov <lbtomov@gmail.com>
2014-02-19 23:06:23 +0200

    timer

--- JaneAusten/JaneAusten/JaneAusten.cs
+++ JaneAusten/JaneAusten/JaneAusten.cs
@@ -9,10 +9,25 @@ namespace JaneAusten
     {
         static void Main()
         {
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 2000;
+            //aTimer.Enabled = true;
+
+            //while (true)
+            //{
+            //    System.Threading.Thread.Sleep(1000);
+            //}
+
             Console.BufferHeight = Console.WindowHeight = 35;      //Remove scrollbar
             Console.BufferWidth = Console.WindowWidth = 100;        //Remove scrollbar
 
             Engine.Run();
+        }
+
+        private static void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now);
         }
     }
 }
