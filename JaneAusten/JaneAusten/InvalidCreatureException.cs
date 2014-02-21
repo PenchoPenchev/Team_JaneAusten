83b74a4da4d716b6aa7fa163bb99ec2c49c33bae
AVelev <velev.alexander@gmail.com>
2014-02-21 10:09:55 +0200

    Added an empty abstract Level class to be inherited by the various levels. Will be completed with next commit.
    Also created an exception class to be used within the Level class

---
+++ JaneAusten/JaneAusten/InvalidCreatureException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JaneAusten
+{
+    public class InvalidCreatureException :ApplicationException
+    {
+
+        public InvalidCreatureException(string message)
+            : base(message)
+        { }
+
+        public InvalidCreatureException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
---
+++ JaneAusten/JaneAusten/Level.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JaneAusten
+{
+    public abstract class Level
+    {
+  
+        // public static List<Enemy> CreateEnemiesList(int numberOfEnemies) //todo
+
+        // public static List<Bonus> CreateBonusesList(int numberOfBonuses) //todo
+
+        // public static void RemoveAllDeadEnemies()
+
+      
+    }
+}
