b2fa3a881f1db31f94072ca8ac5a00a4ef659dcd
LachezarTomov <lbtomov@gmail.com>
2014-02-22 19:04:20 +0200

    Add hiding and showing bonuses with timer event

--- JaneAusten/JaneAusten/JaneAusten.cs
+++ JaneAusten/JaneAusten/JaneAusten.cs
@@ -9,25 +9,19 @@ namespace JaneAusten
     {
         static void Main()
         {
-            System.Timers.Timer aTimer = new System.Timers.Timer();
-            aTimer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimedEvent);
-            aTimer.Interval = 2000;
-            //aTimer.Enabled = true;
+           
 
             //while (true)
             //{
             //    System.Threading.Thread.Sleep(1000);
             //}
 
-            Console.BufferHeight = Console.WindowHeight = 36;      //Remove scrollbar
+            Console.BufferHeight = Console.WindowHeight = 35;      //Remove scrollbar
             Console.BufferWidth = Console.WindowWidth = 100;        //Remove scrollbar
 
             Engine.Run();
         }
 
-        private static void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            Console.WriteLine(DateTime.Now);
-        }
+
     }
 }
