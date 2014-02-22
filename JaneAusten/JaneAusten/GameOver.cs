fe3f45e98864cc26d558adfdb63ffd7ef6b34677
gabrielavg <gabriela_geo@abv.bg>
2014-02-23 00:42:06 +0200

    Add new game or exit option when game over

--- JaneAusten/JaneAusten/GameOver.cs
+++ JaneAusten/JaneAusten/GameOver.cs
@@ -6,7 +6,7 @@ using System.IO;
 
 namespace JaneAusten
 {
-    public class GameOver 
+    public class GameOver
     {
         public static void Display()
         {
@@ -23,16 +23,45 @@ namespace JaneAusten
 "
                     ;
 
-                string bunny = sr.ReadToEnd().ToString();              
+                string bunny = sr.ReadToEnd().ToString();
 
                 Console.SetCursorPosition(0, 0);
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
+
                 Console.WriteLine(text);
-                Console.WriteLine("\n                            Your Score: {0}", Engine.score);
-                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\t\t\t\tYour Score: {0}", Engine.score);
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine(bunny);
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Press enter to start new game and escape to exit.");
+                while (true)
+                {
+                    if (Console.KeyAvailable)
+                    {
+
+                        ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                        while (Console.KeyAvailable)
+                        {
+                            Console.ReadKey(true);
+                        }
+
+                        if (pressedKey.Key == ConsoleKey.Enter)
+                        {
+                            Console.Clear();
+                            StartMenu.DrawMenu();
+                        }
+                        else if (pressedKey.Key == ConsoleKey.Escape)
+                        {
+                            Console.Clear();
+                            Console.SetCursorPosition(0, 0);
+                            break;
+                        }
+                    }
+                }
+
             }
         }
-
     }
 }
