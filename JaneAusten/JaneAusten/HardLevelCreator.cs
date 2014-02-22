428e378bc30fab13552247a5b14a84dd0d40e570
AVelev <velev.alexander@gmail.com>
2014-02-22 02:15:14 +0200

    Added structure for a "Factory "Design Pattern and additional details on level generation.

---
+++ JaneAusten/JaneAusten/HardLevelCreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JaneAusten
+{
+    public class HardLevelCreator : LevelFactory
+    {
+        public override Level GenerateLevel()
+        {
+            var hard = new HardLevel();
+
+            return hard;
+        }
+    }
+}
---
+++ JaneAusten/JaneAusten/LevelFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JaneAusten
+{
+    public abstract class LevelFactory
+    {
+        public abstract Level GenerateLevel();
+    }
+}
