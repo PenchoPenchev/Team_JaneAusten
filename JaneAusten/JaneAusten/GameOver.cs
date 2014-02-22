8dc5854d312a881f2ad2ea95204ae21ccbbea3a3
gabrielavg <gabriela_geo@abv.bg>
2014-02-22 23:47:01 +0200

    Add Game Over message

--- JaneAusten/JaneAusten/GameOver.cs
+++ JaneAusten/JaneAusten/GameOver.cs
@@ -12,10 +12,25 @@ namespace JaneAusten
         {
             using (StreamReader sr = new StreamReader(@"../../Content/GAME_OVER.txt"))
             {
-                string text = sr.ReadToEnd().ToString();              
+                string text =
+@"
+██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗ 
+██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗
+██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝
+██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║
+ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝
+"
+                    ;
+
+                string bunny = sr.ReadToEnd().ToString();              
 
                 Console.SetCursorPosition(0, 0);
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine(text);
+                Console.WriteLine("\n                            Your Score: {0}", Engine.score);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(bunny);
             }
         }
 
