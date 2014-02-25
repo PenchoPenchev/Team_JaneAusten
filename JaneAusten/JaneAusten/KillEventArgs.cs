a414a42c141d0662317b44fb106c41ea1518278e
ivanszbg <ivanszbg@yahoo.com>
2014-02-26 00:39:19 +0200

    EventArgs

---
+++ JaneAusten/JaneAusten/KillEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JaneAusten
+{
+    public class KillEventArgs : EventArgs
+    {
+
+        public KillEventArgs(int score)
+        {
+            this.Score = score;
+        }
+
+        public int Score { get; private set; }
+    }
+}
